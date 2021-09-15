2fc9aaafbb804944b101b132819847be49a905f5
Josh Suson <joshsuson@Joshs-MacBook-Pro.local>
2021-09-15 16:00:24 -0400

    working

--- src/logic.js
+++ src/logic.js
@@ -41,8 +41,8 @@ function move(gameState) {
 
   // TODO: Step 1 - Don't hit walls.
   // Use information in gameState to prevent your Battlesnake from moving beyond the boundaries of the board.
-  // const boardWidth = gameState.board.width
-  // const boardHeight = gameState.board.height
+  const boardWidth = gameState.board.width;
+  const boardHeight = gameState.board.height;
 
   // TODO: Step 2 - Don't hit yourself.
   // Use information in gameState to prevent your Battlesnake from colliding with itself.
